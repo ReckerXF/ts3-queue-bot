326bd782b35dd3e4af0232f870736b211f2dad8c
Michael G <mgoudreault1999@gmail.com>
2023-06-04 22:26:49 -0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -24,8 +24,8 @@ const Config: ConfigImport = {
                     channel: "12510", // The channel the users will be queueing for.
                     excludedSubChannels: ["1613", "1619", "2111", "11501", "23933"], // Subchannels the bot will not look into when seeing if the channel is able to be joined.
                     freezeQueueChannels: [ // Channels that will hold up the queue if there are people in them. (NOT the queued channel)
-                        "Channel 3",
-                        "Channel 4"
+                        "1234",
+                        "1234"
                     ]
                 }
             ],
@@ -43,8 +43,8 @@ const Config: ConfigImport = {
                 "1423"
             ],
             skippedChannels: [ // Channels the bot will ignore people being in and move the queue as normal. (i.e. if someone in queue is in one of these channels they'll be skipped over)
-                "Channel 1",
-                "Channel 2"
+                "1234",
+                "1234"
             ]
         }
     },
@@ -71,8 +71,8 @@ const Config: ConfigImport = {
                     channel: "12510", // The channel the users will be queueing for.
                     excludedSubChannels: ["1613", "1619", "2111", "11501", "23933"], // Subchannels the bot will not look into when seeing if the channel is able to be joined.
                     freezeQueueChannels: [ // Channels that will hold up the queue if there are people in them. (NOT the queued channel)
-                        "Channel 3",
-                        "Channel 4"
+                        "1234",
+                        "1234"
                     ]
                 }
             ],
@@ -90,8 +90,8 @@ const Config: ConfigImport = {
                 "2602"
             ],
             skippedChannels: [ // Channels the bot will ignore people being in and move the queue as normal. (i.e. if someone in queue is in one of these channels they'll be skipped over)
-                "Channel 1",
-                "Channel 2"
+                "1234",
+                "1234"
             ],
         }
     }
